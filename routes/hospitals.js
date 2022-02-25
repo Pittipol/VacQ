e10f166fa131bc7a1fa43094a744a12fcb4380e4
Pittipol Kantavat <pittipol@gmail.com>
2022-02-25 18:19:47 +0700

    add appointment

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const { route } = require('express/lib/application');
 const {getHospitals, getHospital, createHospital, updateHospital, deleteHospital} = require('../controllers/hospitals');
+
+//Include other resource routers
+const appointmentRouter=require('./appointments');
+
 const {protect,authorize} = require('../middleware/auth');
-
-
 const router = express.Router();
 
 const app=express();
+
+//Re-route into other resource routers
+router.use('/:hospitalId/appointments/',appointmentRouter);
 
 router.route('/').get(getHospitals).post(protect, authorize('admin'), createHospital);
 router.route('/:id').get(getHospital).put(protect, authorize('admin'), updateHospital).delete(protect, authorize('admin'), deleteHospital);
